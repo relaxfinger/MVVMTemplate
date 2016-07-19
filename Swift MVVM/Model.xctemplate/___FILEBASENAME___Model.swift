10845c83ab6e3d32209591d614b4f5535e91f1e0
zhang jipeng <relaxfinger@gmail.com>
2016-07-19 18:31:05 +0800

    modify

---
+++ Swift MVVM/Model.xctemplate/___FILEBASENAME___Model.swift
@@ -0,0 +1,26 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import Foundation
+import ObjectMapper 
+import Then
+
+class ___FILEBASENAMEASIDENTIFIER___Model: Mappable {
+    // add properties here
+    // var id: String?
+ 
+    required init?(_ map: Map) {
+    }
+    
+    func mapping(map: Map) {
+        // id     <- map["id"]
+        // id     <- (map["id"], TransformOf<String, Int>(fromJSON: { String($0!) }, toJSON:{ Int($0!)})
+        // time   <- (map["time"], DateFormatterTransform(dateFormatter:NSDateFormatter().then { $0.dateFormat = "YYYY-MM-dd HH:mm:ss" }))
+    }
+}
+
